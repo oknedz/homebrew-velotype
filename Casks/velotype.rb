b24080f0141ad734ee046ae3854213c45035ca18
Zdenko <vasiczdenko@gmail.com>
2026-06-02 19:03:31 +0700

    Use versioned URL instead of latest

--- Casks/velotype.rb
+++ Casks/velotype.rb
@@ -1,8 +1,8 @@
 cask "velotype" do
-  version :latest
-  sha256 :no_check
+  version "0.9.60"
+  sha256 "37021c2119218fa3a92cb2d33dc787c56a6d595125e18ee0a487a890b7e3a452"
 
-  url "https://github.com/oknedz/Velotype_app/releases/latest/download/Velotype.dmg"
+  url "https://github.com/oknedz/Velotype_app/releases/download/v#{version}/Velotype.dmg"
   name "Velotype"
   desc "macOS dictation & AI transcription"
   homepage "https://velotype.app"
